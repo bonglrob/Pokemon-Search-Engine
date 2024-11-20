848ba30a9353c9eb862886796f8a7a8ba82b4fd8
Robert Bonglamphone <rob.bonglamphone@gmail.com>
2024-11-19 22:22:19 -0800

    scraped more text and added new pokedex index

--- pokedex_scraper.py
+++ pokedex_scraper.py
@@ -13,6 +13,14 @@ if response.status_code == 200:
     pokemons = []
     cards = soup.find_all('div', class_='infocard')
     for idx, card in enumerate(cards):
+        # if idx == 3:
+        #     print('two entry uploaded')
+        #     break
+
+        # extract image
+        img = card.find('img')
+        src = img.get('src')
+        alt_text = img.get('alt')
 
         # Extract name
         name = card.find('a', class_='ent-name').text
@@ -21,7 +29,7 @@ if response.status_code == 200:
         types = [t.text for t in card.find_all('a', class_='itype')]
 
         # Extract dex number
-        dex_number = card.find('span', class_='infocard-lg-data text-muted').text.strip()
+        dex_number = card.find('span', class_='infocard-lg-data text-muted').find('small').text.strip()
 
         # Extract description
         description_url = card.find('a', class_='ent-name')['href']
@@ -36,15 +44,29 @@ if response.status_code == 200:
         else:
             description = "Oops! No Pokedex information is available at this time."
 
-        # Combine fields into a single text document
-        text = f"Dex Number: {dex_number} Name: {name} Type: {', '.join(types)} Description: {description}"
-        pokemons.append({'docno': int(idx + 1), 'text': text})
+        # get all text from the page
+        text = description_soup.get_text(separator="\n")
+
+        no_space_text = '\n'.join([line.strip() for line in text.split('\n') if line.strip()])
+        no_space_text += f"Dex Number: {dex_number} Name: {name} Type: {', '.join(types)} Description: {description}"
+
+        # add pokemon row entry
+        pokemons.append({
+            'docno': int(idx + 1), 
+            'name': name, 
+            'dexno': dex_number, 
+            'url': f'https://bulbapedia.bulbagarden.net/wiki/{name}_(Pok%C3%A9mon)', 
+            'img': src, 
+            'type': ', '.join(types), 
+            'description': description, 
+            'text': no_space_text
+        })
 
     print("Scraping Done...")
 
     # Create pokedex corpus
     df = pd.DataFrame(pokemons)
-    df.to_csv('pokemon_data.csv', index=False)
+    df.to_csv('pokemon_full.csv', index=False)
     print("Corpus prepared!")
 else:
     print("Failed to retrieve the page!")
